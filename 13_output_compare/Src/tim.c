7e2deab29fa4c1496f9aa9922da8a973c8ce0c54
michaelcbarilla <michaelcbarilla@gmail.com>
2021-12-30 12:26:56 -0500

    enable clock access to GPIOA, Set PA0 to alternate function mode at 2mhz, set output compare toggle mode, enable tim2 ch1 compare mode

--- 13_output_compare/Src/tim.c
+++ 13_output_compare/Src/tim.c
@@ -7,8 +7,11 @@
 
 #include "tim.h"
 
+#define GPIOAEN		(1U<<2)
 #define TIM2EN		(1U<<0)
 #define CR1_CEN		(1U<<0)
+#define OC_TOGGLE	(1U<<4) | (1U<<5)
+#define CCER_CC1E	(1U<<0)
 
 void tim2_1hz_init(void)
 {
@@ -29,8 +32,17 @@ void tim2_1hz_init(void)
 
 }
 
-void tim2_output_compare(void)
+void tim2_pa0_output_compare(void)
 {
+	/* Enable clock access to GPIOA */
+	RCC->APB2ENR |= GPIOAEN;
+
+	/* Set PA0 mode to alternate function */
+	GPIOA->CRL |= (1U<<3);
+	GPIOA->CRL &= ~(1U<<2);
+	GPIOA->CRL |= (1U<<1);
+	GPIOA->CRL &= ~(1U<<0);
+
 	/* Enable clock access to tim2 */
 	RCC->APB1ENR |= TIM2EN;
 
@@ -39,6 +51,12 @@ void tim2_output_compare(void)
 
 	/* Set auto-reload value */
 	TIM2->ARR = 10000 - 1;			// 10 000 / 10 000 = 1
+
+	/* Set output compare toggle mode */
+	TIM2->CCMR1 = OC_TOGGLE
+			;
+	/* Enable tim2 ch1 to compare mode */
+	TIM2->CCER |= CCER_CC1E;
 
 	/* Clear counter */
 	TIM2->CNT = 0;
